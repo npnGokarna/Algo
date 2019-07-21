749f5c674b43eb3ce487169561524f82d23fb3e8
Gokarna Neupane <gokarnaneupane@Gokarnas-MacBook-Pro.local>
2019-07-21 01:38:58 -0500

    count unique values in a sorted array

---
+++ countUniqueValues.js
@@ -0,0 +1,23 @@
+//count unique values in a sorted array
+
+function countUniqueValues(arr) {
+    if (arr.length===0) {
+        return 0;
+    }
+    let i = 0, j = 1;
+    while(j<arr.length) {
+        if(arr[i] === arr[j]) {
+            ++j;
+        } else {
+            arr.splice(i+1, 0, arr[j]);
+            ++i;
+            ++j;
+        }
+    }
+    return i+1;
+}
+
+console.log(countUniqueValues([1,1,1,1,1,2]));
+console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]));
+console.log(countUniqueValues([]));
+console.log(countUniqueValues([-2,-1,-1,0,1]));
