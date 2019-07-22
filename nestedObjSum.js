e8d6aead4454e452a463b5f1f7a2d83d4d57f69c
Gokarna Neupane <npn.gokarna@gmail.com>
2019-07-21 21:06:41 -0500

    nested object even sum

---
+++ nestedObjSum.js
@@ -0,0 +1,35 @@
+function nestedEvenSum (obj) {
+  let sum = 0;
+  for(let item in obj) {
+      if(obj[item] % 2 === 0) {
+          sum += obj[item];
+      } else if (typeof obj[item] === 'object') {
+          sum += nestedEvenSum(obj[item]);
+      }
+  }
+  return sum;
+}
+
+
+var obj1 = {
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: "yup"
+    }
+  }
+}
+
+var obj2 = {
+  a: 2,
+  b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+  c: {c: {c: 2}, cc: 'ball', ccc: 5},
+  d: 1,
+  e: {e: {e: 2}, ee: 'car'}
+};
+
+console.log(nestedEvenSum(obj1)); // 6
+console.log(nestedEvenSum(obj2)); // 10
